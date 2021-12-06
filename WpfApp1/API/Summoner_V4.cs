0debd9db141b06ef5136016e1c3bed4bf748a7b5
menard49 <amenard@knights.ucf.edu>
2021-12-06 16:10:27 -0500

    update 1.2

---
+++ WpfApp1/API/Summoner_V4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.API
+{
+    public class Summoner_V4 : Api
+    {
+        public Summoner_V4(string region) : base(region)
+        {
+
+        }
+
+        public SummonerDTO GetSummonerByName(string SummonerName)
+        {
+            string path = "summoner/v4/summoners/by-name/" + SummonerName;
+        }
+    }
+}
---
+++ WpfApp1/Model/SummonerDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class SummonerDTO
+    {
+        public int ProfileIconId { get; set; }
+        public string Name { get; set; }
+        public long SummonerLevel { get; set; }
+        public string Id { get; set; }
+
+
+    }
+}
